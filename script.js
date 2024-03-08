5f0a0b586ee398b981d6eb2208707d879fde695e
visalan <visalanoffl@gmail.com>
2024-03-08 11:28:33 +0530

    almost final commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const passbox=document.getElementById("passwordd");
-const length=10;
 const upp="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const low="abcdefghijklmnopqrstuvwxyz";
 const num="0123456789";
@@ -9,19 +8,26 @@ const allchars=upp+low+num+sym;
 function crt()
 {
     let password="";
-
+    var length=Number(document.getElementById("lengthh").value);
     password+=upp[Math.floor(Math.random() * upp.length)];
     password+=low[Math.floor(Math.random() * low.length)];
     password+=num[Math.floor(Math.random() * num.length)];
     password+=sym[Math.floor(Math.random() * sym.length)];
 
-    while(length>=password.length)
+    while(length>password.length)
     {
         password+=allchars[Math.floor(Math.random() * allchars.length)];
     }
+    console.log(length);
     // console.log(password); // Log the generated password to check its value
     // console.log(passbox); 
 
     passbox.value=password;
-
+    
 }
+    function copypass()
+    {
+        passbox.select();
+        document.execCommand("copy");
+    }
+
